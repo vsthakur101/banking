d64c0fb5fe3afb8be6eb15fa9d239c8fea5b854f
Vidhya <vsthakur.it0@gmail.com>
2024-07-13 18:26:13 +0530

    Side bar implemantation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
   const loggedIn = {
     firstName: "Vidhya",
+    lastName: "Thakur",
+    email: "vsthakur@hotmail.com",
   };
   return (
     <section className="home">
@@ -22,7 +25,13 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT SECTION
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+      />
     </section>
   );
 };
